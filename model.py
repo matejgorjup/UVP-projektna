18bcb89456124a65b5a56e0c4d891553a0ff0c2b
Matej Gorjup <matej.gorjup123@gmail.com>
2022-07-11 16:33:51 +0200

    Dodal class Uporabnik

--- model.py
+++ model.py
@@ -1,4 +1,61 @@
 from datetime import date
+import json
+
+#class Uporabnik:
+#    def __init__(self, mid, geslo, stanje):
+#        self.mid = mid
+#        self.geslo = geslo
+#        self.stanje = stanje
+#    
+#    @staticmethod
+#    def registracija(mid, geslo):
+#        if Uporabnik.iz_datoteke(mid) is not None:
+#            raise ValueError("Uporabnik s to MID številko že obstaja! Poskusite se prijaviti.")
+#        else:
+#            ustvarjen = Uporabnik(mid, geslo)
+#            ustvarjen.v_datoteko()
+#            return ustvarjen
+#
+#    @staticmethod
+#    def prijava(mid, vneseno_geslo):
+#        uporabnik = Uporabnik.iz_datoteke(mid)
+#        if uporabnik is None:
+#            raise ValueError("Uporabnik s to MID številko še ne obstaja! Najprej se morate registrirati.")
+#        elif uporabnik.vneseno_geslo == uporabnik.geslo:
+#            return uporabnik
+#        else:
+#            raise ValueError("Geslo je napačno! Poskusite znova.")
+#
+#    @staticmethod
+#    def stanje_uporabnika(mid):
+#        return f"{mid}.json"
+#
+#    def v_slovar(self):
+#        return {
+#            "mid": self.mid,
+#            "geslo": self.geslo,
+#            "stanje": self.stanje.v_slovar()
+#        }
+#
+#    @staticmethod
+#    def iz_slovarja(slovar):
+#        mid = slovar["mid"]
+#        geslo = slovar["geslo"]
+#        return Uporabnik(mid, geslo)
+#
+#    def v_datoteko(self):
+#        with open(Uporabnik.stanje_uporabnika(self.mid), "w") as dat:
+#            json.dump(self.v_slovar(), dat, ensure_ascii=False)
+#
+#    @staticmethod
+#    def iz_datoteke(mid):
+#        try:
+#            with open(Uporabnik.stanje_uporabnika(mid)) as dat:
+#                slovar = json.load(dat)
+#                return Uporabnik.iz_slovarja(slovar)
+#        except FileNotFoundError:
+#            return None
+#
 
 class Govedo:
     def __init__(self, id, datum_rojstva, spol, pasma, datum_prihoda, datum_odhoda):
