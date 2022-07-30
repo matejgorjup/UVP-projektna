6296fa98b29c9bb41e2cc1be55b20c8e1e2fec84
Matej Gorjup <matej.gorjup123@gmail.com>
2022-07-31 00:22:58 +0200

    Popravki

---
+++ primer.py
@@ -0,0 +1,58 @@
+import datetime
+from datetime import date
+from model import Gospodarstvo, Zival, Lokacija, Delavec, Dobrina
+
+Hlipink = Gospodarstvo(100475958, None)
+Ciko = Zival("SI12341234", "Ciko", datetime.date(2018, 12, 23), "M", "LIM", datetime.date(2018, 12, 23), None, None) 
+Belka = Zival("SI12341237", "Belka", datetime.date(2018, 12, 23), "Ž", "LIM", datetime.date(2018, 12, 23), None, None) 
+Lina = Zival("SI36925814", "Lina", datetime.date(2021, 12, 23), "Ž", "LIM", datetime.date(2021, 12, 23), Belka.id, Ciko.id) 
+
+Brezovca = Lokacija("Brezovca", 123)
+Stala = Lokacija("Stala", 3)
+
+Hlipink.prihod_zivali(Belka)
+Hlipink.prihod_zivali(Ciko)
+Hlipink.prihod_zivali(Lina)
+
+Hlipink.dodaj_lokacijo(Brezovca)
+Hlipink.dodaj_lokacijo(Stala)
+
+Brezovca.dodaj_zival(Lina)
+Brezovca.dodaj_zival(Ciko)
+Stala.dodaj_zival(Belka)
+
+Robert = Delavec("Robert")
+Dantes = Delavec("Dantes")
+
+Hlipink.dodaj_delavca(Robert)
+Hlipink.dodaj_delavca(Dantes)
+
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 23), 7, 0)
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 24), 2, 1)
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 25), 5, 0)
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 26), 6, 0)
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 27), 2, 0)
+Robert.dodaj_delovni_dan(datetime.date(2022, 7, 28), 5, 0)
+
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 23), 8, 2)
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 24), 3, 1)
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 25), 0, 0)
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 26), 8, 1)
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 27), 2, 0)
+Dantes.dodaj_delovni_dan(datetime.date(2022, 7, 28), 3, 1)
+
+
+gnoj = Dobrina("gnoj", "m3")
+bale = Dobrina("bale", "kos")
+drva = Dobrina("drva", "m3")
+
+Hlipink.dodaj_dobrino(gnoj)
+Hlipink.dodaj_dobrino(bale)
+Hlipink.dodaj_dobrino(drva)
+
+gnoj.dodaj(45)
+bale.dodaj(105)
+drva.dodaj(205)
+#Dantes.povzetek_ur(datetime.date(2022, 7, 23), datetime.date(2022, 7, 28))
+
+Hlipink.v_datoteko("test")
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,14 +1,54 @@
 import bottle
-import model
+from model import Gospodarstvo, Zival
+from primer import Hlipink
+
+#DATOTEKA_S_STANJEM = "neki.json"
+#
+#try:
+#    ...
 
 @bottle.get("/")
 def osnovna_stran():
-    return bottle.template("osnovna_stran.html")
+    return bottle.template('osnovna_stran.html')
 
-@bottle.get("/register")
+@bottle.get("/register/")
 def register():
+    if 'id' in bottle.request.query:
+        Hlipink.prihod_zivali(bottle.request.query.getunicode['id'])
     return bottle.template(
         "stalez_zivine.html",
-        stalez = model.Matej.register
+        stanje = Hlipink.register
     )
+
+@bottle.post("/register/dodaj-zival/")
+def dodaj_zival():
+    Hlipink.prihod_zivali(bottle.request.forms.getunicode("id"))
+    #Hlipink.shrani_stanje
+    bottle.redirect("/register/")
+
+#@bottle.get("/register/uspesno-dodano/")
+#def uspesno_dodano():
+#    return "Žival uspešno vnešena."
+
+@bottle.get("/delovne-ure/")
+def delovne_ure():
+    return bottle.template('delovne_ure.html')
+
+@bottle.get("/denarno-porocilo/")
+def denarno_porocilo():
+    return bottle.template('denarno_porocilo.html')
+
+@bottle.get("/surovine/")
+def surovine():
+    return bottle.template('surovine.html')
+
+@bottle.get("/lokacija-zivine/")
+def lokacija_zivine():
+    return bottle.template('lokacija_zivine.html')
+
+@bottle.get("/bulma/")
+def bulma():
+    return bottle.template('bulma.html')
+
+
 bottle.run(reloader=True, debug=True)
