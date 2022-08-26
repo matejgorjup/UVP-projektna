51dcf7afd4c137acce450455063b6622e93b83a6
Matej Gorjup <matej.gorjup123@gmail.com>
2022-08-26 23:59:06 +0200

    Popravil in dodal nekatere obrazce

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,72 +1,75 @@
 import bottle
 from datetime import datetime
-from model import Gospodarstvo, Lokacija, Zival, Delavec, DelovniDan, Dobrina, nerazporejene_zivali, najdi_zival, najdi_lokacijo, st_lokacij
+from model import Gospodarstvo, Lokacija, Zival, Delavec, DelovniDan, nerazporejene_zivali, najdi_zival, najdi_lokacijo, st_lokacij
 
 ################################################################################
 
 COOKIE_MID = "mid"
 SECRET = "strogo zaupno"
 
+################################################################################
 
-
-
+def stanje_trenutnega_uporabnika():
+    mid = bottle.response.get_cookie(COOKIE_MID, secret=SECRET)
+    if mid:
+        return Gospodarstvo.iz_datoteke(mid)
+    else:
+        bottle.redirect("/prijava/")
 
 def shrani_stanje(mid):
     mid.v_datoteko()
 
-#def stanje_trenutnega_uporabnika():
-#    mid = bottle.response.get_cookie(COOKIE_MID, secret=SECRET)
-#    if mid:
-#        return Gospodarstvo.iz_datoteke(f"/stanja_uporabnikov/{mid}")
-#    else:
-#        bottle.redirect("/prijava/")
+################################################################################
 
 @bottle.get("/registracija/")
-def registracija_get():
-    return bottle.template("registracija.html", napaka=None, mid=None)
+def registracija():
+    return bottle.template("registracija.html", error=None, mid=None)
 
-#@bottle.post("/registracija/")
-#def registracija_post():
-#    mid = bottle.request.forms.getunicode("mid")
-#    geslo = bottle.request.forms.getunicode("geslo")
-#    if not mid:
-#        return bottle.template("signup.html", napaka="Vnesite MID številko", mid=None)
-#    elif not password:
-#        return bottle.template("signup.html", napaka="Vnesi geslo!", mid=None)
-#    try:
-#        Gospodarstvo.registracija(mid, geslo)
-#        bottle.response.set_cookie(COOKIE_MID, mid, path="/", secret=SECRET)
-#        bottle.redirect("/")
-#    except ValueError as e:
-#        return bottle.template("registracija.tpl", napaka=e.args[0], uporabnik=None)
-#
-@bottle.get("/login/")
-def login():
-    return bottle.template("login.html", napaka=None, uporabnik=None)
+@bottle.post("/registracija/")
+def registracija_post():
+    mid = bottle.request.forms.getunicode("mid")
+    geslo = bottle.request.forms.getunicode("geslo")
+    geslo_check = bottle.request.forms.getunicode("geslo_check")
 
-#@bottle.post("/login/")
-#def prijava():
-#  mid = bottle.request.forms["mid"]
-#  geslo = bottle.request.forms["geslo"]
-#  if model.preveri_uporabnika(mid, geslo): #model.preveri_uporabnika!!
-#      bottle.response.set_cookie(COOKIE_MID, mid, path="/", secret=SECRET)
-#      bottle.redirect("/")
-#  else:
-#    return bottle.template("login.html", napaka="Vnesite mid!", uporabnik=None)
-#
-#@bottle.get("/odjava/")
-#def odjava_get():
-#    uporabnik = stanje_trenutnega_uporabnika()
-#    return bottle.template('odjava.tpl', mid=uporabnik)
-#
-#@bottle.post("/logout/")
-#def odjava():
-#    bottle.response.delete_cookie(COOKIE_MID, mid, path="/")
-#    bottle.redirect("/")
+    if not mid:
+        return bottle.template("registracija.html", error="Vnesite MID številko", mid=None)
+    elif not geslo or not geslo_check:
+        return bottle.template("registracija.html", error="Vnesite gesli!", mid=None)
+    elif geslo != geslo_check:
+        return bottle.template("registracija.html", error="Gesli se ne ujemata!", mid=None)
+    try:
+        Gospodarstvo.registracija(mid, geslo)
+        bottle.response.set_cookie(COOKIE_MID, mid, path="/", secret=SECRET)
+        bottle.redirect("/")
+    except ValueError as err:
+        return bottle.template("registracija.html", error=err.args[0])
+
+@bottle.get("/prijava/")
+def prijava():
+    return bottle.template("prijava.html", error=None, uporabnik=None)
+
+@bottle.post("/prijava/")
+def prijava_post():
+    mid = bottle.request.forms.getunicode("mid")
+    geslo = bottle.request.forms.getunicode("geslo")
+
+    if not mid:
+        return bottle.template("prijava.html", error="Vnesite mid ali pa se registrirajte!", uporabnik=None)
+    try:
+        Gospodarstvo.prijava(mid, geslo)
+        bottle.response.set_cookie(COOKIE_MID, mid, path="/", secret=SECRET)
+        bottle.redirect("/")
+    except ValueError as err:
+        return bottle.template("prijava.html", error=err.args[0])
+
+@bottle.get("/odjava/")
+def odjava():
+    bottle.response.delete_cookie(COOKIE_MID, path="/")
+    bottle.redirect("/prijava/")
 
 ################################################################################
 
-stanje = Gospodarstvo.iz_datoteke("stanja_uporabnikov/100475958")
+stanje = Gospodarstvo.iz_datoteke(100475958)
 
 ################################################################################
 
@@ -84,7 +87,7 @@ def namizje():
 
 @bottle.get("/register/")
 def register():
-    #    stanje = stanje_trenutnega_uporabnika()
+#    stanje = stanje_trenutnega_uporabnika()
     return bottle.template(
         'register.html', 
         register_sorted = sorted(stanje.register, key=lambda x: x.rojstvo) 
@@ -92,16 +95,16 @@ def register():
 
 @bottle.post("/register/")
 def odstrani_zival():
+#    stanje = stanje_trenutnega_uporabnika()
     id = bottle.request.forms.getunicode("id_zivali")
     zival = najdi_zival(id, stanje.register)
     stanje.odhod_zivali(zival)
 
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
+    shrani_stanje(stanje)
     bottle.redirect("/register/")
 
 @bottle.get("/register/dodaj-zival/")
 def dodaj_zival():
-    #    stanje = stanje_trenutnega_uporabnika()
     return bottle.template(
         'register_dodaj_zival.html'
     )
@@ -119,14 +122,15 @@ def dodaj_zival():
     prihod = datetime.strptime(bottle.request.forms.getunicode("datum_prihoda"), '%Y-%m-%d').date()
 
     stanje.prihod_zivali(Zival(id, ime, rojstvo, spol, pasma, prihod, mati, oce, None))
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
 
+    shrani_stanje(stanje)
     bottle.redirect("/register/")
 
 ################################################################################
 
 @bottle.get("/lokacije/")
 def lokacija():
+#    stanje = stanje_trenutnega_uporabnika()
     return bottle.template(
         'lokacije.html',
         lokacije = stanje.lokacije, 
@@ -136,35 +140,38 @@ def lokacija():
 
 @bottle.post("/lokacije/")
 def razporejanje():
+#    stanje = stanje_trenutnega_uporabnika()
     id_zivali = bottle.request.forms.getunicode("id_zivali")
     ime_lokacije = bottle.request.forms.getunicode("ime_lokacije")
     zival = najdi_zival(id_zivali, stanje.register)
     lokacija = najdi_lokacijo(ime_lokacije, stanje.lokacije)
     lokacija.dodaj_zival(zival)
     
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
+    shrani_stanje(stanje)
     bottle.redirect("/lokacije/")
 
 @bottle.post("/lokacije/<lok.ime>")
 def premik():
+#    stanje = stanje_trenutnega_uporabnika()
     id_zivali = bottle.request.forms.getunicode("zival")
     zival = (najdi_zival(id_zivali, stanje.register))
     od = najdi_lokacijo(bottle.request.forms.getunicode("lokacija_from", stanje.lokacije))
     do = najdi_lokacijo(bottle.request.forms.getunicode("lokacija_to", stanje.lokacije))
     od.premakni_zival(do)
 
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
+    shrani_stanje(stanje)
     bottle.redirect("/lokacije/")
 
 @bottle.post("/lokacije/veliki-premik/")
 def veliki_premik():
+#    stanje = stanje_trenutnega_uporabnika()
     odd = bottle.request.forms.getunicode("lokacija_od")
     doo = bottle.request.forms.getunicode("lokacija_do")
     lok1 = najdi_lokacijo(odd, stanje.lokacije)
     lok2 = najdi_lokacijo(doo, stanje.lokacije)
     lok1.premakni_vse(lok2)
 
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
+    shrani_stanje(stanje)
     bottle.redirect("/lokacije/")
 
 @bottle.get("/lokacije/dodaj-lokacijo/")
@@ -175,11 +182,12 @@ def dodaj_lokacijo():
 
 @bottle.post("/lokacije/dodaj-lokacijo/")
 def dodaj_lokacijo():
+#    stanje = stanje_trenutnega_uporabnika()
     ime = bottle.request.forms.getunicode("ime")
     povrsina = bottle.request.forms.getunicode("povrsina")
     stanje.dodaj_lokacijo(Lokacija(ime, povrsina, []))
 
-    Gospodarstvo.v_datoteko(stanje, "stanja_uporabnikov/100475958")
+    shrani_stanje(stanje)
     bottle.redirect("/lokacije/")
 
 
@@ -187,27 +195,44 @@ def dodaj_lokacijo():
 
 @bottle.get("/delovne-ure/")
 def delovne_ure():
+#    stanje = stanje_trenutnega_uporabnika()
     return bottle.template(
         'delovne_ure.html',
         delovna_sila = stanje.delovna_sila
     )
 
-@bottle.post("/delovne-ure/<indeks_delavca:int>/")
-def opravljeno():
+
+
+@bottle.get("/delovne-ure/delavec/<indeks_delavca:int>/")
+def delavec(indeks_delavca):
+#    stanje = stanje_trenutnega_uporabnika()
+    pregled = stanje.delovna_sila[indeks_delavca]
+    return bottle.template(
+        'delavec.html',
+        delavec = pregled
+    )
+
+@bottle.post("/delovne-ure/delavec/<indeks_delavca:int>/")
+def opravljeno(indeks_delavca):
+#    stanje = stanje_trenutnega_uporabnika()
     datum = bottle.request.forms.getunicode("datum")
     ure_kmet = bottle.request.forms.getunicode("ure_kmet")
     ure_gozd = bottle.request.forms.getunicode("ure_gozd")
-    delavec = nekki
+    delavec = stanje.delovna_sila[index_delavca]
     delavec.dodaj_delovni_dan(datum, ure_kmet, ure_gozd)
 
-@bottle.get("/delovne_ure/delavec/<indeks_delavca:int>/")
-def delavec(index_delavca):
-    #uporabnik = trenutni_uporabnik()
-    delavec = stanje.delovna_sila[index_delavca]
-    return bottle.template(
-        "delavec.html",
-    )
-    
+    shrani_stanje(stanje)
+    bottle.redirect("/delovne-ure/delavec/<indeks_delavca:int>/")
+
+@bottle.post("/delovne-ure/dodaj/")    
+def dodaj():
+#    stanje = stanje_trenutnega_uporabnika()
+    ime = bottle.request.forms.getunicode("ime")
+    stanje.dodaj_delavca(Delavec(ime, []))
+
+    shrani_stanje(stanje)
+    bottle.redirect("/delovne-ure/")
+
 ################################################################################
 
 bottle.run(reloader=True, debug=True)
